a470c224a1e61799e03ecef1f48368a76103b0f2
sups149 <sups149@gmail.com>
2019-04-13 12:01:09 +0000

    Adding controller, Dockerfile and change in gradle file.

---
+++ src/main/java/com/test/springbootdemo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.test.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/test")
+    public String getReq() {
+        return "Hello World";
+    }
+}
